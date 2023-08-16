a78d62057ea1ca36ad7d6c5579a8e5ea1f39b6ca
davi-aguiar <daviaguiar2010@hotmail.com>
2023-08-15 23:20:39 -0300

    style: formatting(add grid)

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+/* Código não está funcional (olhar fim do projeto)
 const menuButton = document.querySelector('.menu-button');
 const navList = document.querySelector('.nav-list');
 
 menuButton.addEventListener('click', () => {
     navList.classList.toggle('show');
 });
+*/
